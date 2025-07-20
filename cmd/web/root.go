0827e29e98170af84759e852506df2bb5d774493
Dean Fogarty <angrytongan@gmail.com>
2025-07-20 18:51:13 +1000

    Sizing.

--- cmd/web/root.go
+++ cmd/web/root.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func (app *Application) root(w http.ResponseWriter, r *http.Request) {
 	pageData := map[string]any{
-		"NumChunks": 1,
+		"NumChunks": 5,
 	}
 
 	app.render(w, r, "root", pageData, http.StatusOK)
